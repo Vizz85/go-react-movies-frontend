3a0a150c90f3796bb3d6238089487b717908027f
Alessandro Vizin <alessandro.vizin@valtech.de>
2021-06-19 14:14:58 +0200

    Connect movies and one movie components to backend

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,31 +3,52 @@ import { Link } from "react-router-dom";
 
 export default class Movies extends Component {
 
-    state = {movies: []};
+    state = {
+        movies: [],
+        isLoaded: false,
+        error: null
+    };
 
     componentDidMount() {
-        this.setState({
-            movies: [
-                {id: 1, title: 'The Shawshank Redemption', runtime: 142},
-                {id: 2, title: 'The Godfather', runtime: 175},
-                {id: 3, title: 'The Dark Knight', runtime: 153},
-            ]
-        })
+        fetch("http://localhost:4000/v1/movies")
+            .then(response => {
+                console.log("Status code is", response.status)
+                if (response.status !== 200) {
+                    let err = new Error("Invalid response code "+ response.status)
+                    this.setState({error: err});
+                }
+
+                return response.json()
+            })
+            .then(json => {
+                this.setState({
+                    movies: json.movies,
+                    isLoaded: true
+                })
+            })
     }
 
     render() {
-        return (
-            <Fragment>
-                <h2>Choose a movie</h2>
+        const { movies, isLoaded, error } = this.state;
 
-                <ul>
-                    {this.state.movies.map(m => (
-                        <li key={m.id}>
-                            <Link to={`/movies/${m.id}`}>{m.title}</Link>
-                        </li>
-                    ))}
-                </ul>
-            </Fragment>
-        );
+        if (error) {
+            return <div>Error: {error.message}</div>
+        } else if (!isLoaded) {
+            return <p>Loading...</p>
+        } else {
+            return (
+                <Fragment>
+                    <h2>Choose a movie</h2>
+
+                    <ul>
+                        {movies.map(m => (
+                            <li key={m.id}>
+                                <Link to={`/movies/${m.id}`}>{m.title}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </Fragment>
+            );
+        }
     }
 }
