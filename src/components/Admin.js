63572c204ca589a5f123e94567cffa1beba474d4
Alessandro Vizin <alessandro.vizin@valtech.de>
2021-06-19 20:55:52 +0200

    Add manage movies screen

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,9 +1,51 @@
-import React, { Component } from "react";
+import React, {Component, Fragment} from "react";
+import {Link} from "react-router-dom";
 
 export default class Admin extends Component {
+
+    state = {
+        movies: [],
+        isLoaded: false,
+        error: null
+    };
+
+    componentDidMount() {
+        fetch("http://localhost:4000/v1/movies")
+            .then(response => {
+                if (response.status !== 200) {
+                    let err = new Error("Invalid response code "+ response.status)
+                    this.setState({error: err});
+                }
+
+                return response.json()
+            })
+            .then(json => {
+                this.setState({
+                    movies: json.movies,
+                    isLoaded: true
+                })
+            })
+    }
+
     render() {
-        return (
-            <h2>Admin Area</h2>
-        )
+        const { movies, isLoaded, error } = this.state;
+
+        if (error) {
+            return <div>Error: {error.message}</div>
+        } else if (!isLoaded) {
+            return <p>Loading...</p>
+        } else {
+            return (
+                <Fragment>
+                    <h2>Edit a Movie</h2>
+
+                    <div className="list-group">
+                        {movies.map(m => (
+                            <Link key={m.id} className="list-group-item list-group-item-action" to={`/admin/movie/${m.id}`}>{m.title}</Link>
+                        ))}
+                    </div>
+                </Fragment>
+            );
+        }
     }
 }
